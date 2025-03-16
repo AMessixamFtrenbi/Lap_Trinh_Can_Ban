6392e22b5337a7d5962d3348cd7e2fbbde419be1
AMessixamFtrenbi <tuankietlenhu@gmail.com>
2025-03-17 00:33:13 +0700

    Update Cau lenh for, do...while,while.c

--- Cau lenh for, do...while,while.c
+++ Cau lenh for, do...while,while.c
@@ -219,79 +219,68 @@ int main()
 }
 */
 /*Cau 11:
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-void toBinary(int n)
-{
-    int binary[32];
+void toHe2(int n) {
+    int He2[32];
     int i = 0;
-    while (n > 0)
-    {
-        binary[i] = n % 2;
+    while (n > 0) {
+        He2[i] = n % 2;
         n = n / 2;
         i++;
     }
     printf("He co so 2: ");
-    for (int j = i - 1; j >= 0; j--)
-    {
-        printf("%d", binary[j]);
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%d", He2[j]);
     }
     printf("\n");
 }
 
-void toOctal(int n)
-{
-    int octal[32];
+void toHe8(int n) {
+    int He8[32];
     int i = 0;
-    while (n > 0)
-    {
-        octal[i] = n % 8;
+    while (n > 0) {
+        He8[i] = n % 8;
         n = n / 8;
         i++;
     }
     printf("He co so 8: ");
-    for (int j = i - 1; j >= 0; j--)
-    {
-        printf("%d", octal[j]);
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%d", He8[j]);
     }
     printf("\n");
 }
 
-void toHexadecimal(int n)
-{
-    int hexadecimal[32];
+void toHe16(int n) {
+    char He16[32];
     int i = 0;
-    while (n > 0)
-    {
+    while (n > 0) {
         int remainder = n % 16;
-        if (remainder < 10)
-        {
-            hexadecimal[i] = remainder + '0';
-        }
-        else
-        {
-            hexadecimal[i] = remainder - 10 + 'A';
+        if (remainder < 10) {
+            He16[i] = remainder + '0';
+        } else {
+            He16[i] = remainder - 10 + 'A';
         }
         n = n / 16;
         i++;
     }
     printf("He co so 16: ");
-    for (int j = i - 1; j >= 0; j--)
-    {
-        printf("%c", hexadecimal[j]);
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%c", He16[j]);
     }
     printf("\n");
 }
 
-int main()
-{
+int main() {
     int n;
-    printf("Nhap vao man hinh so n: \n");
+    printf("Nhap vao so: ");
     scanf("%d", &n);
-    toBinary(n);
-    toOctal(n);
-    toHexadecimal(n);
+    toHe2(n);
+    toHe8(n);
+    toHe16(n);
     return 0;
+}
 }*/
 /*Cau 12:
 #include<stdio.h>
